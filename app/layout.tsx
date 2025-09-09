7aff736aa0c6cf730d297991b5313e72566af361
Fred Hilgenfeldt <f.hilgenfeldt@gmail.com>
2025-09-09 21:56:06 +0200

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import '../styles/globals.css';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1 className="text-3xl font-bold text-emerald-600">Tailwind v4 is live</h1>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
